4b5fde0c1f89850cc427a833e1be32b7f254c9b7
Alexandr Arsenyuk <arsenyukofficial@gmail.com>
2019-06-28 12:01:13 +0300

    initial commit

---
+++ OpenWeatherClient/OpenWeatherClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeatherClient
+//
+//  Created by Александр Арсенюк on 28/06/2019.
+//  Copyright © 2019 Александр Арсенюк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
